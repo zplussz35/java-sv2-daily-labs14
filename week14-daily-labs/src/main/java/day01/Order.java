3aec7a51e58320f0bc471db66027676ceabcd011
zplussz35 <15.zoltan.16@gmail.com>
2022-01-27 21:01:42 +0100

    week14 day01 feladat elkészítése

---
+++ week14-daily-labs/src/main/java/day01/Order.java
@@ -0,0 +1,46 @@
+package day01;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private String status;
+    private LocalDate orderDate;
+    private List<Product> products = new ArrayList<>();
+
+
+    public Order(String status, LocalDate orderDate) {
+        this.status = status;
+        this.orderDate = orderDate;
+    }
+
+    public  String sayHyToOrder(Order o){
+        return "Hello "+o;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "status='" + status + '\'' +
+                ", orderDate=" + orderDate +
+                ", products=" + products +
+                '}';
+    }
+}
---
+++ week14-daily-labs/src/main/java/day01/OrderService.java
@@ -0,0 +1,68 @@
+package day01;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderService {
+    private List<Order> orders = new ArrayList<>();
+
+    public void saveOrder(Order order) {
+        orders.add(order);
+    }
+
+    public List<Order> findOrdersByStatus(String status) {
+        return orders.stream().
+                filter(o -> o.getStatus().equals(status))
+                .collect(Collectors.toList());
+    }
+
+    public long countOrdersByStatus(String status) {
+        return orders.stream().
+                filter(o -> o.getStatus().equals(status))
+                .count();
+    }
+
+    public List<Product> findProductsOverPrice(int amount){
+        return orders.stream()
+                .flatMap(o->o.getProducts().stream())
+                .filter(p->p.getPrice()>amount)
+                .distinct()
+                .toList();
+    }
+
+    public String orderToString(Order o){
+        return o.getStatus()+" "+o.getOrderDate().toString();
+    }
+
+    public List<Order> getOrdersBetweenDates(LocalDate start, LocalDate end) {
+        return orders.stream()
+                .filter(o -> o.getOrderDate().isAfter(start) )
+                .filter(o -> o.getOrderDate().isBefore(end))
+                .collect(Collectors.toList());
+    }
+
+    public boolean isOrderWithLessProductThan(int number){
+        return  orders.stream()
+                .mapToInt(o->o.getProducts().size())
+                .anyMatch(i->i<number);
+    }
+
+
+    public Order getOrderWithMaxNumberOfProducts(){
+        return orders.stream()
+                .sorted(Collections.reverseOrder(Comparator.comparing(o->o.getProducts().size())))
+                .findFirst().orElseThrow(()->new IllegalStateException("Empty List"));
+    }
+
+    public List<Order> getOrdersWithProductInCategory(String category){
+        return orders.stream()
+                .filter(o->o.getProducts().
+                        stream().anyMatch(p->p.getCategory().equals(category)))
+                .collect(Collectors.toList());
+    }
+
+}
